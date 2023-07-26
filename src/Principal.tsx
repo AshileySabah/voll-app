866c0cfa8f9b57fcf2b729f58613019bb8eeabb0
AshileySabah <ashileysabrina2002@gmail.com>
2023-07-26 17:20:07 -0300

    feat: tema

---
+++ src/Principal.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text } from "react-native";
+
+export default function Principal() {
+  return <Text>Tela principal</Text>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
